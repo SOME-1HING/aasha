68f18a16a7d46fb53e4f49b0c2b785685b47dfe8
SOME1HING <yashprakash2005@gmail.com>
2024-05-11 15:56:17 +0530

    Added Icons

--- lib/components/bottom_nav.dart
+++ lib/components/bottom_nav.dart
@@ -10,37 +10,75 @@ class BottomNav extends StatefulWidget {
 }
 
 class _BottomNavState extends State<BottomNav> {
-  final IconDecoration = BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.all(Radius.circular(10)),
-  );
+  int active = 1;
+  double iconSize = 84;
+
+  Color getColor(int button) {
+    return active == button ? Color(0xFF4CBC9A) : Color(0xFF979C9E);
+  }
+
+  void setActive(int button) {
+    setState(() {
+      active = button;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
       height: 60,
-      color: Colors.red,
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        InkWell(
-            onTap: () {},
-            child: Container(
-              child: Icon(Icons.home),
-              decoration: IconDecoration,
-            )),
-        InkWell(
-            onTap: () {},
-            child: Container(
-              child: Icon(Icons.healing),
-              decoration: IconDecoration,
-            )),
-        InkWell(
-            onTap: () {},
-            child: Container(
-              child: Icon(Icons.person),
-              decoration: IconDecoration,
-            )),
-      ]),
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 6),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5), // Shadow color
+            spreadRadius: 5, // Spread radius
+            blurRadius: 7, // Blur radius
+            offset: Offset(0, 5), // Offset
+          ),
+        ],
+      ) ,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () {
+              setActive(0);
+            },
+            icon: Image.asset(
+              "./assets/icons/home.png",
+              color: getColor(0),
+              width: iconSize,
+              height: iconSize,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              setActive(1);
+            },
+            icon: Image.asset(
+              "./assets/icons/contri.png",
+              color: getColor(1),
+              width: iconSize,
+              height: iconSize,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              setActive(2);
+            },
+            icon: Image.asset(
+              "./assets/icons/user.png",
+              color: getColor(2),
+              width: iconSize,
+              height: iconSize,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,12 +9,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isNotification = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
-        title: Text('My Widget'),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Image.asset(
+            "./assets/icons/menu.png",
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(
+
+              isNotification?
+              "./assets/icons/notifi2.png":"./assets/icons/notifi1.png" ,
+              scale: 0.7,
+            ),
+          ),
+        ],
+        backgroundColor: Colors.transparent,
       ),
+      backgroundColor: Colors.black,
+
       bottomNavigationBar: BottomNav(myString: "ds"),
     );
   }
