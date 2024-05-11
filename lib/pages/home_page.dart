49ba27082fb8f3b434358c5c6d9bae315c185c88
SOME1HING <yashprakash2005@gmail.com>
2024-05-11 22:43:16 +0530

    Home Page Drawer Initial Commitr

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,6 +18,7 @@ class _HomePageState extends State<HomePage> {
   bool isNotification = false;
   late bool isPanelOpen;
   late final PanelController panelController;
+  final GlobalKey<ScaffoldState> _key = GlobalKey();
 
   final List<Featured> demo = [
     Featured(
@@ -40,6 +41,8 @@ class _HomePageState extends State<HomePage> {
         uid: "fehfiu3ggug2f9udg9ug"),
   ];
 
+  String active = "Home";
+
   @override
   void initState() {
     setState(() {
@@ -58,6 +61,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _key,
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
@@ -294,7 +298,9 @@ class _HomePageState extends State<HomePage> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     IconButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        _key.currentState!.openDrawer();
+                      },
                       icon: Image.asset(
                         color: isPanelOpen ? Colors.black : Colors.white,
                         "./assets/icons/menu.png",
@@ -321,6 +327,215 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.grey,
       bottomNavigationBar: BottomNav(myString: "ds"),
+      drawer: Drawer(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(topRight: Radius.circular(50))),
+        backgroundColor: Color(0xD94CBC9A),
+        child: Padding(
+          padding: EdgeInsets.only(
+              left: 40,
+              top: kToolbarHeight + MediaQuery.of(context).padding.top),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SizedBox(
+                width: 64,
+                height: 64,
+                child: ClipOval(
+                    child: Image.asset(
+                  "./assets/images/home_top.png",
+                  fit: BoxFit.cover,
+                ))),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Rohit Sharma",
+              style: GoogleFonts.prompt(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+            ),
+            Text(
+              "heyrohithh@gmail.com",
+              style: GoogleFonts.prompt(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w200,
+                  letterSpacing: 0.2,
+                  color: Colors.white),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+                    borderRadius: BorderRadius.circular(14),
+                    onTap: () {},
+                    splashColor: Colors.white.withOpacity(0.5),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        color: (active == "Home")
+                            ? Colors.white.withOpacity(0.3)
+                            : Colors.transparent,
+                      ),
+                      padding: EdgeInsets.only(left: 10),
+                      alignment: Alignment.centerLeft,
+                      height: 60,
+                      width: 200,
+                      child: Text(
+                        "Home",
+                        style: GoogleFonts.prompt(
+                            fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 200,
+                    child: Divider(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                  InkWell(
+                    borderRadius: BorderRadius.circular(14),
+                    onTap: () {},
+                    splashColor: Colors.white.withOpacity(0.5),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        color: (active == "Settings")
+                            ? Colors.white.withOpacity(0.3)
+                            : Colors.transparent,
+                      ),
+                      padding: EdgeInsets.only(left: 10),
+                      alignment: Alignment.centerLeft,
+                      height: 60,
+                      width: 200,
+                      child: Text(
+                        "Settings",
+                        style: GoogleFonts.prompt(
+                            fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 200,
+                    child: Divider(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                  InkWell(
+                    borderRadius: BorderRadius.circular(14),
+                    onTap: () {},
+                    splashColor: Colors.white.withOpacity(0.5),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        color: (active == "My Activities")
+                            ? Colors.white.withOpacity(0.3)
+                            : Colors.transparent,
+                      ),
+                      padding: EdgeInsets.only(left: 10),
+                      alignment: Alignment.centerLeft,
+                      height: 60,
+                      width: 200,
+                      child: Text(
+                        "My Activities",
+                        style: GoogleFonts.prompt(
+                            fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 200,
+                    child: Divider(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                  InkWell(
+                    borderRadius: BorderRadius.circular(14),
+                    onTap: () {},
+                    splashColor: Colors.white.withOpacity(0.5),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        color: (active == "About Us")
+                            ? Colors.white.withOpacity(0.3)
+                            : Colors.transparent,
+                      ),
+                      padding: EdgeInsets.only(left: 10),
+                      alignment: Alignment.centerLeft,
+                      height: 60,
+                      width: 200,
+                      child: Text(
+                        "About Us",
+                        style: GoogleFonts.prompt(
+                            fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 200,
+                    child: Divider(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                  InkWell(
+                    borderRadius: BorderRadius.circular(14),
+                    onTap: () {},
+                    splashColor: Colors.white.withOpacity(0.5),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        color: (active == "Help & FAQ")
+                            ? Colors.white.withOpacity(0.3)
+                            : Colors.transparent,
+                      ),
+                      padding: EdgeInsets.only(left: 10),
+                      alignment: Alignment.centerLeft,
+                      height: 60,
+                      width: 200,
+                      child: Text(
+                        "Help & FAQ",
+                        style: GoogleFonts.prompt(
+                            fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            InkWell(
+              onTap: () {},
+              child: SizedBox(
+                height: 50,
+                width: 200,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Log Out",
+                      style:
+                          GoogleFonts.prompt(fontSize: 18, color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ]),
+        ),
+      ),
     );
   }
 }
