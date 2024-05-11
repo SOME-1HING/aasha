01962f555e1e87ecf9d0cb09db9bf09c58b498a4
Shakir <shakirali.sacube@gmail.com>
2024-05-12 04:46:07 +0530

    Added project model

---
+++ lib/module/project_model.dart
@@ -0,0 +1,21 @@
+class ProjectModel {
+  String projectId;
+  String projectName;
+  String projectUrl;
+  String projectDescription;
+  String projectBackdrop;
+  String ngoId;
+  String ngoName;
+  String ngoDp;
+
+  ProjectModel({
+    required this.projectId,
+    required this.projectName,
+    required this.projectUrl,
+    required this.projectDescription,
+    required this.projectBackdrop,
+    required this.ngoId,
+    required this.ngoName,
+    required this.ngoDp,
+  });
+}
